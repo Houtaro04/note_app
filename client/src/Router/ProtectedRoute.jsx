6e36f1d271a1c41c2cad0d793c83c76a508212f6
Houtaro <cungvietanh04@gmail.com>
2025-08-04 15:26:56 +0700

    update client

--- client/src/Router/ProtectedRoute.jsx
+++ client/src/Router/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import React, { Children } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
-export default function ProtectedRoute(children) {
+export default function ProtectedRoute() {
     console.log({ accessToken: localStorage.getItem('accessToken') });
     if(!localStorage.getItem('accessToken')) {
         return <Navigate to='/login' />;
